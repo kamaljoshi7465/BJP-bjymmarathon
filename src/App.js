19c2bf90fe8207856a5e7e66d95856d81fe740d9
Anand K <akkit1648@gmail.com>
2025-09-17 17:54:03 +0000

    App.js edited online with Bitbucket

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from './Pages/HomePage';
-import RegistrationForm from './Pages/RegistrationForm';
 import Gorakhpur from './Pages/Media/Gorakhpur';
 import Meerut from './Pages/Media/Meerut';
 function App() {
@@ -9,7 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/register" element={<RegistrationForm />} /> */}
+    
         <Route path="/media/gorakhpur" element={<Gorakhpur />} />
         <Route path="/media/meerut" element={<Meerut />} />
       </Routes>
